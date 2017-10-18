4162496e13f05ee968418c9ced7c038ec9909dc9
Rita Rusetskaya <r.rusetskaya@gmail.com>
2017-10-18 16:57:07 +0300

    Assembly created,signed with strong name and added to GAC

---
+++ NET.W.2017.Rusetskaya.01/FirstAssembly/FirstAssembly/Client.cs
@@ -0,0 +1,16 @@
+﻿using FirstAssembly;
+using System;
+class Client
+{
+    static void Main()
+    {
+        NewPerson obj = new NewPerson();
+        obj.NewPersonInfo();
+
+        // Здесь будет загружаться модуль Person.netmodule
+        Person obj1 = new Person();
+        obj1.AboutPerson();
+        Console.ReadLine();
+    }
+
+}
---
+++ NET.W.2017.Rusetskaya.01/FirstAssembly/FirstAssembly/NewPerson.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstAssembly
+{
+    public class NewPerson
+    {
+        public void NewPersonInfo()
+        {
+            Person person = new Person("Rita", "Rusetskaya", 20);
+            Console.WriteLine(person.AboutPerson());
+            Console.ReadLine();
+        }
+    }
+}

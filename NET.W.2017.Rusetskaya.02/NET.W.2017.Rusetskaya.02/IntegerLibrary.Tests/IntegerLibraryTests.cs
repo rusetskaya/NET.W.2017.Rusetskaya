f6958874c15b2e4805eb471b496900755295af25
Rita Rusetskaya <r.rusetskaya@gmail.com>
2017-10-22 00:29:10 +0300

    Add NextBiggerNumber method && NUnit tests for it

---
+++ NET.W.2017.Rusetskaya.02/NET.W.2017.Rusetskaya.02/IntegerLibrary.Tests/IntegerLibraryTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static IntegerLibrary.SpecialIntegerLogic;
+using NUnit.Framework;
+
+namespace IntegerLibrary.Tests
+{
+    public class IntegerLibraryTests
+    {
+        [TestCase(12, ExpectedResult = 21)]
+        [TestCase(513, ExpectedResult = 531)]
+        [TestCase(2017, ExpectedResult = 2071)]
+        [TestCase(414, ExpectedResult = 441)]
+        [TestCase(144, ExpectedResult = 414)]
+        [TestCase(1234321, ExpectedResult = 1241233)]
+        [TestCase(1234126, ExpectedResult = 1234162)]
+        [TestCase(3456432, ExpectedResult = 3462345)]
+        [TestCase(10, ExpectedResult = -1)]
+        [TestCase(20, ExpectedResult = -1)]
+
+        public long NextBiggerNumberTest(long actual)
+        {
+            return NextBiggerNumber(actual);
+        }
+    }
+}
+
+
+
+
+//namespace IntegerLibrary.Tests
+
+//{
+
+//    [TestFixture]
+
+//    public class NUnitTests
+
+//    {
+
+//        [Test]
+
+//        public void SumOfTwoNumbers()
+
+//        {
+
+//            Assert.AreEqual(10, 5 + 5);
+
+//        }
+
+
+
+//        [Test]
+
+//        public void AreTheValuesTheSame()
+
+//        {
+
+//            Assert.AreSame(10, 5 + 6);
+
+//        }
+
+//    }
+
+//}
---
+++ NET.W.2017.Rusetskaya.02/NET.W.2017.Rusetskaya.02/IntegerLibrary/SpecialIntegerLogic.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegerLibrary
+{
+    public static class SpecialIntegerLogic
+    {
+        #region NextBiggerNumberMethods
+        public static long NextBiggerNumber(long number)
+        {
+            if (number < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(number));
+            }
+            if (IsNumberDecrease(number))
+            {
+                return -1;
+            }
+            
+            string sNumber = ToSortedString(number);
+            long nextNumber = ++number;
+            string sNext = ToSortedString(nextNumber);
+            while (sNumber != sNext)
+            {
+                sNext = ToSortedString(++nextNumber);
+            }
+            return nextNumber;
+
+        }
+        private static bool IsNumberDecrease(long number)
+        {
+            int prevDigit = (int)number % 10;
+            int nextDigit = 0;
+            number = number / 10;
+
+            while (number != 0)
+            {
+                nextDigit = (int)number % 10;
+                if (prevDigit > nextDigit)
+                {
+                    return false;
+                }
+                number = number / 10;
+                prevDigit = nextDigit;
+
+            }
+            return true;
+        }
+        private static string ToSortedString(long number)
+        {
+            char[] array = number.ToString().ToCharArray();
+            Array.Sort(array);
+            return new String(array);
+        }
+        #endregion
+    }
+}

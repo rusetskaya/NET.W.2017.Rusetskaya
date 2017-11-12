ba776d8ad60d367f482454f72db6edae5b60ed58
Rita Rusetskaya <r.rusetskaya@gmail.com>
2017-11-12 16:24:36 +0300

    Just refactor 05 project with StyleCop

--- NET.W.2017.Rusetskaya.05/NET.W.2017.Rusetskaya.05/ArrayLibrary/ArraySort.cs
+++ NET.W.2017.Rusetskaya.05/NET.W.2017.Rusetskaya.05/ArrayLibrary/ArraySort.cs
@@ -8,6 +8,34 @@ namespace ArrayLibrary
 {
     public class ArraySort
     {
+        /// <summary>
+        /// Delegate (should contain compare)
+        /// </summary>
+        /// <param name="lhs"></param>
+        /// <param name="rhs"></param>
+        /// <returns></returns>
+        public delegate int CompareDlgt(int[] lhs, int[] rhs);
+
+        /// <summary>
+        /// Swap
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        public static void Swap<T>(ref T a, ref T b)
+        {
+            T t = a;
+            a = b;
+            b = t;
+        }
+
+        /// <summary>
+        /// Sum of Row El-s
+        /// </summary>
+        /// <param name="array"></param>
+        /// <returns></returns>
+        public static int SumOfRowElemets(int[] array) => array.Sum();
+
         /*
         public interface IComparer
         {
@@ -15,7 +43,51 @@ namespace ArrayLibrary
         }
         */
 
-        public delegate int CompareDlgt(int[] lhs, int[] rhs);
+        public static class ArrayHelper
+        {
+            /// <summary>
+            /// Sort
+            /// </summary>
+            /// <param name="jaggedArray"></param>
+            /// <param name="comparer"></param>
+            public static void BubbleSort(int[][] jaggedArray, IComparer<int[]> comparer)
+            {
+                if (jaggedArray == null)
+                {
+                    throw new ArgumentNullException(nameof(jaggedArray));
+                }
+
+                for (int i = 1; i < jaggedArray.Length; i++)
+                {
+                    for (int j = 0; j < jaggedArray.Length - 1; j++)
+                    {
+                        if (comparer.Compare(jaggedArray[j], jaggedArray[j + 1]) > 0)
+                        {
+                            Swap(ref jaggedArray[j], ref jaggedArray[j + 1]);
+                        }
+                    }
+                }
+            }
+
+            public static void BubbleSortWithDlgt(int[][] jaggedArray, CompareDlgt cdlgt)
+            {
+                if (jaggedArray == null)
+                {
+                    throw new ArgumentNullException(nameof(jaggedArray));
+                }
+
+                for (int i = 1; i < jaggedArray.Length; i++)
+                {
+                    for (int j = 0; j < jaggedArray.Length - 1; j++)
+                    {
+                        if (cdlgt(jaggedArray[j], jaggedArray[j + 1]) > 0)
+                        {
+                            Swap(ref jaggedArray[j], ref jaggedArray[j + 1]);
+                        }
+                    }
+                }
+            }
+        }
 
         /// <summary>
         /// Compare sum by inc
@@ -39,6 +111,7 @@ namespace ArrayLibrary
                 {
                     return -1;
                 }
+
                 return lhs.Sum() - rhs.Sum();
             }
         }
@@ -69,6 +142,7 @@ namespace ArrayLibrary
                 return rhs.Sum() - lhs.Sum();
             }
         }
+
         /// <summary>
         /// compare max by inc
         /// </summary>
@@ -86,13 +160,16 @@ namespace ArrayLibrary
                 {
                     return 1;
                 }
+
                 if (rhs == null)
                 {
                     return -1;
                 }
+
                 return lhs.Max() - rhs.Max();
             }
         }
+
         /// <summary>
         /// compare max by dec
         /// </summary>
@@ -110,13 +187,16 @@ namespace ArrayLibrary
                 {
                     return -1;
                 }
+
                 if (rhs == null)
                 {
                     return 1;
                 }
+
                 return rhs.Max() - lhs.Max();
             }
         }
+
         /// <summary>
         /// compare min by inc
         /// </summary>
@@ -134,13 +214,16 @@ namespace ArrayLibrary
                 {
                     return 1;
                 }
+
                 if (rhs == null)
                 {
                     return -1;
                 }
+
                 return lhs.Min() - rhs.Min();
             }
         }
+
         /// <summary>
         /// compare min by dec
         /// </summary>
@@ -158,76 +241,14 @@ namespace ArrayLibrary
                 {
                     return -1;
                 }
+
                 if (rhs == null)
                 {
                     return 1;
                 }
+
                 return rhs.Min() - lhs.Min();
             }
         }
-
-        public static class ArrayHelper
-        {
-            /// <summary>
-            /// Sort
-            /// </summary>
-            /// <param name="jaggedArray"></param>
-            /// <param name="comparer"></param>
-            public static void BubbleSort(int[][] jaggedArray, IComparer<int[]> comparer)
-            {
-                if (jaggedArray == null)
-                {
-                    throw new ArgumentNullException(nameof(jaggedArray));
-                }
-                for (int i = 1; i < jaggedArray.Length; i++)
-                {
-                    for (int j = 0; j < jaggedArray.Length - 1; j++)
-                    {
-                        if (comparer.Compare(jaggedArray[j], jaggedArray[j + 1]) > 0)
-                        {
-                            Swap(ref jaggedArray[j], ref jaggedArray[j + 1]);
-                        }
-                    }
-                }
-            }
-
-            public static void BubbleSortWithDlgt(int[][] jaggedArray, CompareDlgt cdlgt)
-            {
-                if (jaggedArray == null)
-                {
-                    throw new ArgumentNullException(nameof(jaggedArray));
-                }
-                for (int i = 1; i < jaggedArray.Length; i++)
-                {
-                    for (int j = 0; j < jaggedArray.Length - 1; j++)
-                    {
-                        if (cdlgt(jaggedArray[j], jaggedArray[j + 1]) > 0)
-                        {
-                            Swap(ref jaggedArray[j], ref jaggedArray[j + 1]);
-                        }
-                    }
-                }
-            }
-        }
-
-        /// <summary>
-        /// Swap
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="a"></param>
-        /// <param name="b"></param>
-        public static void Swap<T>(ref T a, ref T b)
-        {
-            T t = a;
-            a = b;
-            b = t;
-        }
-
-        /// <summary>
-        /// Sum of Row El-s
-        /// </summary>
-        /// <param name="array"></param>
-        /// <returns></returns>
-        public static int SumOfRowElemets(int[] array) => array.Sum();
     }
 }
--- NET.W.2017.Rusetskaya.05/NET.W.2017.Rusetskaya.05/PolynomLibrary/Polynomial.cs
+++ NET.W.2017.Rusetskaya.05/NET.W.2017.Rusetskaya.05/PolynomLibrary/Polynomial.cs
@@ -8,9 +8,27 @@ namespace PolynomLibrary
 {
     public class Polynomial : ICloneable
     {
-        #region Private fields and props
         private static int power;
         private double[] coefficients = new double[power];
+
+        #region Constrs
+        public Polynomial(double[] array)
+        {
+            power = array.Length - 1;
+            ////for (int i = 0; i < power; i++)
+            ////    Сoefficients[i] = array[i];
+            Сoefficients = array;
+        }
+
+        ~Polynomial()
+        {
+            //// Do unmanaged resource clean up
+
+            Console.WriteLine("Is destructor");
+        }
+        #endregion
+
+        #region Props
         private double[] Сoefficients
         {
             get
@@ -38,90 +56,8 @@ namespace PolynomLibrary
         }
         #endregion
         
-        #region Constrs
-        public Polynomial(double[] array)
-        {
-            power = array.Length - 1;
-            //for (int i = 0; i < power; i++)
-            //    Сoefficients[i] = array[i];
-            Сoefficients = array;
-        }
-        ~Polynomial()
-        {
-            // Do unmanaged resource clean up
-
-            Console.WriteLine("Is destructor");
-        }
-        #endregion
-
         #region Public Methods
-        public object Clone()
-        {
-            return new Polynomial(coefficients);
-            //return (Polynomial)MemberwiseClone();
-        }
-
-        #region Overrided Objects virtual methods
-        public override bool Equals(object obj)
-        {
-            if (obj == null)
-                return false;
-            Polynomial polinom = obj as Polynomial;
-            if (polinom as Polynomial == null)
-                return false;
-
-            return polinom.Power == Power && polinom.coefficients.SequenceEqual(coefficients);
-        }
-
-        public bool Equals(Polynomial polinom)
-        {
-            if (ReferenceEquals(polinom,null))
-                return false;
-            return polinom.Power == Power && polinom.coefficients.SequenceEqual(coefficients);
-        }
-
-        public override int GetHashCode()
-        {
-            int code = (int)coefficients.Sum();
-            return Power + code;
-        }
-
-        public override string ToString()
-        {
-            String result = "";
-            for (int i = 0; i < coefficients.Length; i++)
-            {
-                string sign;
-                
-                if (coefficients[i]<0)
-                {
-                    sign = "";
-                }
-                else
-                {
-                    sign = "+";
-                }
-                result += sign + coefficients[i].ToString() + "x^" + i.ToString();
-            }
-            if (result[0] == '+')
-            {
-                result = result.Substring(1);
-            }
-
-            return result.ToString();
-        }
-
-
-        //protected override void Finalize()
-        //{
-        //    // Do unmanaged resource clean up
-        //}
-
-
-        //Перегружаем бинарный оператор +
-
-        #endregion
-
+        
         #region Overrided operations
         public static Polynomial operator +(Polynomial firstPolinom, Polynomial secondPolinom)
         {
@@ -137,17 +73,22 @@ namespace PolynomLibrary
                         arrayResult[i] += secondPolinom.coefficients[i];
                         continue;
                     }
+
                     if (i >= secondPolinom.coefficients.Length)
                     {
                         arrayResult[i] += firstPolinom.coefficients[i];
                         continue;
                     }
+
                     arrayResult[i] = firstPolinom.coefficients[i] + secondPolinom.coefficients[i];
                 }
 
                 return new Polynomial(arrayResult);
             }
-            else throw new ArgumentNullException(nameof(firstPolinom));
+            else
+            {
+                throw new ArgumentNullException(nameof(firstPolinom));
+            }
         }
 
         // Перегружаем бинарный оператор - 
@@ -165,18 +106,22 @@ namespace PolynomLibrary
                         arrayResult[i] -= secondPolinom.coefficients[i];
                         continue;
                     }
+
                     if (i >= secondPolinom.coefficients.Length)
                     {
                         arrayResult[i] += firstPolinom.coefficients[i];
                         continue;
                     }
+
                     arrayResult[i] = firstPolinom.coefficients[i] - secondPolinom.coefficients[i];
                 }
 
                 return new Polynomial(arrayResult);
             }
-            else throw new ArgumentNullException(nameof(firstPolinom));
-
+            else
+            {
+                throw new ArgumentNullException(nameof(firstPolinom));
+            }
         }
 
         public static Polynomial operator *(Polynomial firstPolinom, Polynomial secondPolinom)
@@ -190,15 +135,16 @@ namespace PolynomLibrary
                 {
                     for (int j = 0; j < secondPolinom.coefficients.Length; j++)
                     {
-
                         arrayResult[i + j] += firstPolinom.coefficients[i] * secondPolinom.coefficients[j];
                     }
                 }
 
                 return new Polynomial(arrayResult);
             }
-            else throw new ArgumentNullException(nameof(firstPolinom));
-
+            else
+            {
+                throw new ArgumentNullException(nameof(firstPolinom));
+            }
         }
 
         public static bool operator ==(Polynomial firstPolinom, Polynomial secondPolinom)
@@ -208,8 +154,84 @@ namespace PolynomLibrary
 
         public static bool operator !=(Polynomial firstPolinom, Polynomial secondPolinom)
         {
-            return !(firstPolinom.Equals(secondPolinom));
+            return !firstPolinom.Equals(secondPolinom);
         }
+        #endregion
+
+        public object Clone()
+        {
+            return new Polynomial(coefficients);
+            ////return (Polynomial)MemberwiseClone();
+        }
+
+        #region Overrided Objects virtual methods
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+
+            Polynomial polinom = obj as Polynomial;
+            if (polinom as Polynomial == null)
+            {
+                return false;
+            }
+
+            return polinom.Power == Power && polinom.coefficients.SequenceEqual(coefficients);
+        }
+
+        public bool Equals(Polynomial polinom)
+        {
+            if (ReferenceEquals(polinom, null))
+            {
+                return false;
+            }
+
+            return polinom.Power == Power && polinom.coefficients.SequenceEqual(coefficients);
+        }
+
+        public override int GetHashCode()
+        {
+            int code = (int)coefficients.Sum();
+            return Power + code;
+        }
+
+        public override string ToString()
+        {
+            string result = string.Empty;
+            for (int i = 0; i < coefficients.Length; i++)
+            {
+                string sign;
+
+                if (coefficients[i] < 0)
+                {
+                    sign = string.Empty;
+                }
+                else
+                {
+                    sign = "+";
+                }
+
+                result += sign + coefficients[i].ToString() + "x^" + i.ToString();
+            }
+
+            if (result[0] == '+')
+            {
+                result = result.Substring(1);
+            }
+
+            return result.ToString();
+        }
+
+        /*
+        //protected override void Finalize()
+        //{
+        //    // Do unmanaged resource clean up
+        //}
+        */
+
+        ////Перегружаем бинарный оператор +
         #endregion
 
         #endregion
@@ -221,11 +243,15 @@ namespace PolynomLibrary
             {
                 return false;
             }
+
             if (ReferenceEquals(firstPolinom, null) && ReferenceEquals(secondPolinom, null))
             {
                 return false;
             }
-            else return true;
+            else 
+            {
+                return true;
+            }
         }
 
         private static int CountResultPower(int firstPower, int secondPower)

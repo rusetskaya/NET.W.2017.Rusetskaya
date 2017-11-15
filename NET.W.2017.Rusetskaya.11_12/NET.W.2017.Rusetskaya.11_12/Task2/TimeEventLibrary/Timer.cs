8ebd2c0ceacb69c5d67a07baaf7ed0233bfda3b0
Rita Rusetskaya <r.rusetskaya@gmail.com>
2017-11-15 18:10:28 +0300

    Added 2-nd task: time-event

---
+++ NET.W.2017.Rusetskaya.11_12/NET.W.2017.Rusetskaya.11_12/Task2/TimeEventLibrary/Timer.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TimeEventLibrary
+{
+    #region TimerEventArgs
+    public sealed class TimerEventArgs : EventArgs
+    {
+        private readonly string message;
+        private int time;
+
+        public TimerEventArgs(int time, string message = "")
+        {
+            if (ReferenceEquals(message, null))
+            {
+                throw new ArgumentNullException(nameof(message));
+            }
+            else
+            {
+                this.message = message;
+            }
+
+            if (time > 0)
+            {
+                this.time = time;
+            }
+        }
+
+        public string Message => message;
+        public int Time => time;
+    }
+    #endregion
+
+    #region MessageManager
+    public class MessageManager
+    {
+        public delegate void NewTimerEventHandler(object sender, TimerEventArgs timerEventArgs);
+
+        public event NewTimerEventHandler NewMessage;
+
+        protected virtual void OnNewMessage(object sender, TimerEventArgs timerEventArgs)
+        {
+            if (!ReferenceEquals(NewMessage,null))
+            {
+                Thread.Sleep(timerEventArgs.Time);
+                NewMessage(sender, timerEventArgs);
+            }
+        }
+
+        public void SimulateNewMessage(int time, string message)
+        {
+            OnNewMessage(this, new TimerEventArgs(time, message));
+        }
+    }
+    #endregion
+
+    #region Listeners
+    public sealed class FirstSubscriber
+    {
+        public FirstSubscriber(MessageManager message)
+        {
+            message.NewMessage += FirstSubMsg;
+        }
+
+        public FirstSubscriber()
+        {
+        }
+
+        public void Register(MessageManager message)
+        {
+            message.NewMessage += FirstSubMsg;
+        }
+
+        private void FirstSubMsg(object sender, TimerEventArgs eventArgs)
+        {
+            Console.WriteLine($"First subscriber message: {eventArgs.Message}, time: {eventArgs.Time}");
+        }
+
+        public void Unregister(MessageManager message)
+        {
+            message.NewMessage -= FirstSubMsg;
+        }
+    }
+
+    public class SecondSubscriber
+    {
+        public SecondSubscriber(MessageManager message)
+        {
+            message.NewMessage += SecondSubMsg;
+        }
+
+        public SecondSubscriber()
+        {
+        }
+
+        private void SecondSubMsg(Object sender, TimerEventArgs eventArgs)
+        {
+            Console.WriteLine($"Second subscriber message: {eventArgs.Message}, time: {eventArgs.Time}");
+        }
+
+        public void Unregister(MessageManager message)
+        {
+            message.NewMessage -= SecondSubMsg;
+        }
+
+        public void Register(MessageManager message)
+        {
+            message.NewMessage += SecondSubMsg;
+        }
+    }
+    #endregion
+}
---
+++ NET.W.2017.Rusetskaya.11_12/NET.W.2017.Rusetskaya.11_12/Task2/TimeEventRunner/Runner.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using TimeEventLibrary;
+
+namespace TimeEventRunner
+{
+    class Runner
+    {
+        static void Main(string[] args)
+        {
+            int time;
+            Console.WriteLine("Enter time value, please");
+            time = Convert.ToInt32(Console.ReadLine());
+
+            var firstSubscriber = new FirstSubscriber();
+            var secondSubscriber = new SecondSubscriber();
+            var manager = new MessageManager();
+
+            firstSubscriber.Register(manager);
+            manager.SimulateNewMessage(time, "1-st event");
+
+            secondSubscriber.Register(manager);
+            manager.SimulateNewMessage(time, "2-nd event");
+
+            firstSubscriber.Unregister(manager);
+
+            var secManager = new MessageManager();
+            firstSubscriber.Register(secManager);
+
+            manager.SimulateNewMessage(time, "3-rd event");
+            secManager.SimulateNewMessage(time,"4-th event");
+
+            Console.ReadLine();
+        }
+    }
+}

e9e6400c3e355d989ffe0d189f5a056e3af71c87
Rita Rusetskaya <r.rusetskaya@gmail.com>
2017-11-15 20:47:10 +0300

    Use styleCop in tasks

--- NET.W.2017.Rusetskaya.11_12/NET.W.2017.Rusetskaya.11_12/Task3/BinarySearchLibrary/AdapterComparer.cs
+++ NET.W.2017.Rusetskaya.11_12/NET.W.2017.Rusetskaya.11_12/Task3/BinarySearchLibrary/AdapterComparer.cs
@@ -1,7 +1,19 @@
-﻿namespace BinarySearchLibrary
+﻿using System;
+using System.Collections.Generic;
+
+namespace BinarySearchLibrary
 {
-    public class AdapterComparer
+    public class AdapterComparer<T> : IComparer<T>
     {
-        
+        private Comparison<T> comparison;
+
+        public AdapterComparer(Comparison<T> comparison)
+        {
+            this.comparison = comparison;
+        }
+        public int Compare(T x, T y)
+        {
+            return this.comparison(x, y);
+        }
     }
 }

80d0d99c1ac45189500f7ce1574f2c6b34b1bae1
Rita Rusetskaya <r.rusetskaya@gmail.com>
2017-11-28 12:56:26 +0300

    Task 6

---
+++ NET.W.2017.Rusetskaya.Test/Task6.Tests/TestGenerator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using NUnit.Framework;
+using Test6.Solution;
+using static Test6.Solution.Generator;
+
+namespace Task6.Tests
+{
+    [TestFixture]
+    public class TestGenerator
+    {
+        [Test]
+        public void Is_For_6_is_1_1_2_3_5_8_13_21()
+        {
+            bool temp;
+            int i = 0;
+            Count1<int> counter = new Count1<int>();
+            List<int> list = new List<int>() {2, 3, 5, 8 , 13, 21};
+            foreach (var fib in GetNumbers(6, 1, 1, counter.GetSum))
+            {
+                temp = (fib == list[i]);
+                i++;
+                Debug.WriteLine(fib);
+            }
+        }
+
+        
+    }
+}
---
+++ NET.W.2017.Rusetskaya.Test/Test6.Solution/Generator.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+
+namespace Test6.Solution
+{
+    public class Generator
+    {
+        public delegate T Count<T>(T prev, T curr);
+        public static IEnumerable<T> GetNumbers<T>(int n, T a, T b, Count<T> rule)
+        {
+            T prev = a;
+            T next = b;
+            for (int i = 0; i < n; i++)
+            {
+                T sum = rule.Invoke(prev, next);
+                prev = next;
+                next = sum;
+                yield return sum;
+            }
+        }
+
+    }
+
+    
+    public class Count1<T> : ISum<T>
+    {
+        public T GetSum(T prev, T curr)
+        {
+            if (typeof(T) == typeof(int))
+            {
+                object sum = Convert.ToInt32(prev.ToString()) + Convert.ToInt32(curr.ToString());
+                return (T)sum;
+            }
+
+            if (typeof(T) == typeof(double))
+            {
+                object sum = Convert.ToDouble(prev.ToString()) + Convert.ToDouble(curr.ToString());
+                return (T)sum;
+            }
+             throw new Exception($"Format exception {typeof(T)}");
+        }
+    }
+
+    public class Count2<T> : ISum<T>
+    {
+        public T GetSum(T prev, T curr)
+        {
+            if (typeof(T) == typeof(int))
+            {
+                object sum = 6 * Convert.ToInt32(curr.ToString()) - 8 * Convert.ToInt32(prev.ToString());
+                return (T)sum;
+            }
+
+            if (typeof(T) == typeof(double))
+            {
+                object sum = 6 * Convert.ToDouble(curr.ToString()) + 8 * Convert.ToDouble(prev.ToString());
+                return (T)sum;
+            }
+            throw new Exception($"Format exception {typeof(T)}");
+        }
+    }
+
+    public class Count3<T> : ISum<T>
+    {
+        public T GetSum(T prev, T curr)
+        {
+            if (typeof(T) == typeof(int))
+            {
+                object sum = Convert.ToInt32(curr.ToString()) + Convert.ToInt32(prev.ToString())/ Convert.ToInt32(curr.ToString());
+                return (T)sum;
+            }
+
+            if (typeof(T) == typeof(double))
+            {
+                object sum = Convert.ToDouble(curr.ToString()) + Convert.ToDouble(prev.ToString()) / Convert.ToInt32(curr.ToString());
+                return (T)sum;
+            }
+            throw new Exception($"Format exception {typeof(T)}");
+        }
+    }
+
+    public interface ISum<T>
+    {
+        T GetSum(T prev, T curr);
+    }
+}

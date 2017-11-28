e743a13af7fc83a3b909634e461449c2b076f7f9
Rita Rusetskaya <r.rusetskaya@gmail.com>
2017-11-28 11:17:16 +0300

    Task 2

---
+++ NET.W.2017.Rusetskaya.Test/Task2.Solution/RandomGenerator.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Task2.Solution
+{
+    public abstract class RandomGenerator
+    {
+        public string WorkingDirectory;
+
+        public string FileExtension;
+
+        protected RandomGenerator(string working, string file)
+        {
+            WorkingDirectory = working;
+            FileExtension = file;
+        }
+        public void GenerateFiles(int filesCount, int contentLength)
+        {
+            for (var i = 0; i < filesCount; ++i)
+            {
+                var generatedFileContent = this.GenerateFileContent(contentLength);
+
+                var generatedFileName = $"{Guid.NewGuid()}{this.FileExtension}";
+
+                this.WriteBytesToFile(generatedFileName, generatedFileContent);
+            }
+        }
+
+        protected abstract byte[] GenerateFileContent(int contentLength);
+
+        private void WriteBytesToFile(string fileName, byte[] content)
+        {
+            if (!Directory.Exists(WorkingDirectory))
+            {
+                Directory.CreateDirectory(WorkingDirectory);
+            }
+
+            File.WriteAllBytes($"{WorkingDirectory}//{fileName}", content);
+        }
+
+        
+    }
+
+    public class RandomCharsFileGenerator : RandomGenerator
+    {
+
+        public RandomCharsFileGenerator(string working, string file) : base(working, file){ }
+
+        protected override byte[] GenerateFileContent(int contentLength)
+        {
+            var generatedString = this.RandomString(contentLength);
+
+            var bytes = Encoding.Unicode.GetBytes(generatedString);
+
+            return bytes;
+        }
+
+        private string RandomString(int Size)
+        {
+            var random = new Random();
+
+            const string input = "abcdefghijklmnopqrstuvwxyz0123456789";
+
+            var chars = Enumerable.Range(0, Size).Select(x => input[random.Next(0, input.Length)]);
+
+            return new string(chars.ToArray());
+        }
+    }
+
+    public class RandomBytesFileGenerator : RandomGenerator
+    {
+        public RandomBytesFileGenerator(string working, string file) : base(working, file){ }
+
+        protected override byte[] GenerateFileContent(int contentLength)
+        {
+            var random = new Random();
+
+            var fileContent = new byte[contentLength];
+
+            random.NextBytes(fileContent);
+
+            return fileContent;
+        }
+    }
+}
---
+++ NET.W.2017.Rusetskaya.Test/Task2.Tests/Program.cs
@@ -0,0 +1,12 @@
+﻿using Task2.Solution;
+namespace Task2.Tests
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            RandomGenerator gen1 = new RandomBytesFileGenerator("Files with random chars", ".txt");
+            RandomGenerator gen2 = new RandomCharsFileGenerator("Files with random bytes", ".bytes");
+        }
+    }
+}

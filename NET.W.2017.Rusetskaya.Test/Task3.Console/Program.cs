dc051d0bcbd1733efa08ce55ca66ac8b794ec120
Rita Rusetskaya <r.rusetskaya@gmail.com>
2017-11-28 10:58:33 +0300

    Task 3

--- NET.W.2017.Rusetskaya.Test/Task3.Console/Program.cs
+++ NET.W.2017.Rusetskaya.Test/Task3.Console/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Task3.Solution;
 
 namespace Task3.Console
 {
@@ -10,6 +11,17 @@ namespace Task3.Console
     {
         static void Main(string[] args)
         {
+            var manager = new BankEvent();
+            var broker = new Broker(manager);
+            var bank = new Bank(manager);
+            broker.Register(manager);
+            manager.SimulateNewStock(20,23);
+
+            //Thread.Sleep(1000);
+
+            bank.Unregister(manager);
+
+            manager.SimulateNewStock(24,26);
         }
     }
 }
---
+++ NET.W.2017.Rusetskaya.Test/Task3.Solution/BankEvent.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task3.Solution
+{
+    public class BankEvent //Manager
+    {
+        public event EventHandler<BankEventArgs> NewStock = delegate { };
+
+        public void OnNewStock(BankEventArgs e)
+        {
+            EventHandler<BankEventArgs> temp = NewStock;
+            
+            temp?.Invoke(this, e);
+        }
+
+        public void SimulateNewStock(int usd, int euro)
+        {
+            OnNewStock(new BankEventArgs(usd, euro));
+        }
+    }
+
+    public sealed class BankEventArgs : EventArgs
+    {
+        private int usd;
+        private int euro;
+
+        public BankEventArgs(int usd, int euro)
+        {
+            Euro = euro;
+            USD = usd;
+        }
+
+        public int USD { get; set; }
+        public int Euro { get; set; }
+
+    }
+
+    //Listeners
+    public sealed class Broker
+    {
+        public string Name { get; set; }
+
+        public Broker(BankEvent be)
+        {
+            be.NewStock += UpdateBroker;
+        }
+
+        public void Register(BankEvent be)
+        {
+            be.NewStock += UpdateBroker;
+        }
+
+        public void UpdateBroker(object sender, BankEventArgs eventArgs)
+        {        
+            Console.WriteLine($"{eventArgs.Euro} {eventArgs.USD}");
+        }
+
+        public void Unregister(BankEvent be)
+        {
+            be.NewStock -= UpdateBroker;
+        }
+    }
+
+    public class Bank
+    {
+        public string Name { get; set; }
+
+        public Bank(BankEvent be)
+        {
+            be.NewStock += UpdateBank;
+        }
+
+        public void Register(BankEvent be)
+        {
+            be.NewStock += UpdateBank;
+        }
+
+        public void UpdateBank(object sender, BankEventArgs eventArgs)
+        {
+            Console.WriteLine($"{eventArgs.Euro} {eventArgs.USD}");
+        }
+
+        public void Unregister(BankEvent be)
+        {
+            be.NewStock -= UpdateBank;
+        }
+    }
+}
